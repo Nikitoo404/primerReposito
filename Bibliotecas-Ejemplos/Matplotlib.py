81f747896a24ee52b53893ac0ff7ab029fb672ed
Nikitoo404 <impresonico@gmail.com>
2023-09-07 19:57:41 -0300

    modificacion

--- Bibliotecas-Ejemplos/Matplotlib.py
+++ Bibliotecas-Ejemplos/Matplotlib.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 nombres = ['LLA', 'Juntos por el cambio', 'Unión por la Patria']
-valores = [32.1, 20.9, 26.8]
+valores = [29.86, 28, 27.28]
 
 plt.bar(nombres, valores)
 plt.show()
