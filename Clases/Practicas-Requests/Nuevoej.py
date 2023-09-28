731c7b60b0e12771ccca66200afe1f14807b203f
Nikitoo404 <impresonico@gmail.com>
2023-09-28 20:32:04 -0300

    nuevo ejercicio

--- Clases/Practicas-Requests/Nuevoej.py
+++ Clases/Practicas-Requests/Nuevoej.py
@@ -2,4 +2,21 @@
 
 import requests
 from bs4 import BeautifulSoup
+import sqlite3
 
+url='https://quinielanacional1.com.ar/'
+
+respuesta=requests.get(url)
+
+contenidoHTML=respuesta.content
+
+soup=BeautifulSoup(contenidoHTML,'html.parser')
+
+titulo=soup.title.text
+
+cuerpo=soup.body
+
+divisorRH=cuerpo.find(id='principal')
+
+#print("Título:",titulo)
+print(divisorRH)
