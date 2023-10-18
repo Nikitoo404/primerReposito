0c7f0c420a98f5209a0240823bcdfbdc2a9a1192
Nikitoo404 <impresonico@gmail.com>
2023-10-18 20:22:17 -0300

    cambios

--- Bibliotecas-Ejemplos/Requests.py
+++ Bibliotecas-Ejemplos/Requests.py
@@ -5,5 +5,7 @@ url='https://www.youtube.com/'
 
 respuesta=requests.get(url)
 #si en vez de poner la varible, pongo su contenido, es lo mismo.
-
-print(respuesta.content)
+if respuesta.status_code==200:
+    print(respuesta.content)
+else:
+    print("Error. Revise su conexión a Internet, o si la dirección que proporcionó es la correcta.")
