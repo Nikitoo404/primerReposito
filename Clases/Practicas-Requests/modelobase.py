daa888d0aa3bd5253a1af3185cd3911942fffec6
Nikitoo404 <impresonico@gmail.com>
2023-10-19 16:11:14 -0300

    base-quiniela

---
+++ Clases/Practicas-Requests/modelobase.py
@@ -0,0 +1,57 @@
+import sqlite3
+
+class BaseDatos():
+    def __init__(self):
+        self.bd="BaseQuinela.sqlite" 
+        self.conexion=""
+        self.cursor=""
+        
+    def conectar(self):
+        self.conexion=sqlite3.connect(self.bd)
+        self.cursor = self.conexion.cursor() 
+    
+    def actualizarBD(self,argumento):
+        self.conectar()
+        self.cursor.execute(argumento)
+        self.conexion.commit()
+        self.conexion.close()
+    
+    def consultar(self,argumento,cantidad=0):
+        self.conectar()
+        datos=self.cursor.execute(argumento)
+        if cantidad ==1:
+            fila=datos.fetchone()
+            return fila
+        else:
+            fila=datos.fetchall()
+            return fila
+            
+    def generarTablas(self):
+        self.actualizarBD("""
+                CREATE TABLE IF NOT EXISTS quinela
+                (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nombre TEXT)
+                """)
+
+        self.actualizarBD("""
+                    CREATE TABLE IF NOT EXISTS fecha
+                    (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        fecha DATE)
+                    """)
+
+        self.actualizarBD("""
+                    CREATE TABLE IF NOT EXISTS posicion
+                    (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        posicion INTEGER)
+                    """)
+
+        self.actualizarBD("""
+                    CREATE TABLE IF NOT EXISTS Numeros
+                    (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        id_fecha INTEGER,
+                        id_posicion INTEGER,
+                        id_quinela INTEGER,
+                        valor INTEGER
+                        )
+                    """)
+    
