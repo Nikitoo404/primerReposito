9eb9e6d65ec4cdf465e374398b4d05d1e20e11e2
Nikitoo404 <impresonico@gmail.com>
2023-11-12 13:33:06 -0300

    s

--- pruebaCam.py
+++ pruebaCam.py
@@ -5,11 +5,11 @@ import itertools
 from collections import Counter
 from collections import deque
 
-'import cv2 as cv'
+import cv2 as cv
 import numpy as np
-'import mediapipe as mp'
+import mediapipe as mp
 
-'from utils import CvFpsCalc'
-'from model import KeyPointClassifier'
+from utils import CvFpsCalc
+'from model1 import KeyPointClassifier'
 'from model import PointHistoryClassifier'
 
